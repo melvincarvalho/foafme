d647e12a70acb0f58cc718167c726eb664363a55
melvincarvalho <melvincarvalho@gmail.com>
2009-05-01 14:54:05 +0100

    first release

---
+++ install.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("config.php");
+require_once("db.class.php");
+
+$db = new db_class();
+$db->connect('localhost', $config['db_user'], $config['db_pwd'], $config['db_name']);
+$r = $db->select('CREATE TABLE IF NOT EXISTS `foaf` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(255) NOT NULL,
+  `rdf` blob NOT NULL,
+  `rdf2` blob NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=585 ;');
+
+
+?>
