f01c592889222567f59155c844b99e2282aceb56
phpbbopenid <phpbbopenid@gmail.com>
2009-05-20 23:32:48 +0200

    certificate

--- tabsecurity.php
+++ tabsecurity.php
@@ -1,34 +1,55 @@
 <?
-// Include the SimplePie library, and the one that handles internationalized domain names.
-require_once('lib/libAuthentication.php');
+// includes
+include('config.php');
+include('lib/libAuthentication.php');
 
 $auth = $_SESSION['auth'];
 
 if (!empty($_REQUEST['webid'])) {
-  $auth = get_agent($_REQUEST['webid']);
+	$auth = get_agent($_REQUEST['webid']);
+	$key = $auth['agent']['RSAKey'];
 
+	print "<h3>Security</h3>";
 
-    $key = $auth['agent']['RSAKey'];
+	if (!empty($key)) {
 
-  print "<h3>Security</h3>";
+		// TODO: rdfa to match the table below
+		print "Public Key: $key[modulus]<br/>";
+		print "Exponent: $key[exponent]<br/>";
 
-  if (!empty($key)) {
-
-      print "Public Key: $key[modulus]<br/>";
-      print "Exponent: $key[exponent]<br/>";
-
-  } else {
-     print "No public key found";
-  }
-    print "<h3>Coming soon</h3>  Protect account with SSL certificate<br/> Edit profile (please use tabulator at the moment)<br/> Privacy control<br/>";
+	} else {
+		print "No public key found";
+	}
+	
+	print "<h3>Coming soon</h3>";
+	print "Protect account with SSL <a href=config[certficate_uri]?" .
+	      "commonName=foaf.me-cert&foaf=" .
+	      $_REQUEST['webid'] . "'>certificate</a><br/> ";
+	print "Edit profile (please use tabulator at the moment)<br/> ";
+	print "Privacy control<br/>";
 
 } else {
-?>
-                          <table>
-                          <tr typeof="cert:identity"><td><b>Secure Account!</b> </td><td>(RSA)</td></tr>
-                          <tr><td>Public Key: </td><td><input inner="cert:hex" property="rsa:modulus" id="publicKey" onChange="makeTags()" type="text" name="publicKey" /></tr>
-                          <tr><td>Exponent: </td><td><input inner="cert:decimal" property="rsa:public_exponent" id="exponent" onChange="makeTags()" type="text" name="exponent" /> (Default = 65537)</tr>
-                          </table>
+	?>
+
+<table>
+	<tr typeof="cert:identity">
+		<td><b>Secure Account!</b></td>
+		<td>(RSA)</td>
+	</tr>
+	<tr>
+		<td>Public Key:</td>
+		<td><input inner="cert:hex" property="rsa:modulus" id="publicKey"
+			onChange="makeTags()" type="text" name="publicKey" />
+	
+	</tr>
+	<tr>
+		<td>Exponent:</td>
+		<td><input inner="cert:decimal" property="rsa:public_exponent"
+			id="exponent" onChange="makeTags()" type="text" name="exponent" />
+		(Default = 65537)
+	
+	</tr>
+</table>
 
 
 
