e4eb5680c77930e7583bc73099e892f227a7cf56
phpbbopenid <phpbbopenid@gmail.com>
2009-05-20 22:29:43 +0200

    new modes

--- config.php
+++ config.php
@@ -2,11 +2,16 @@
 
 $config = array(
         /* db */
-        'db_name'    => '',
-        'db_user'    => '',
-        'db_pwd'     => '',
+        'db_name'        => '',
+        'db_user'        => '',
+        'db_pwd'         => '',
         /* store */
-        'store_name' => 'arc_tests',
+        'store_name'     => 'arc_tests', // temporary table name
+
+        /* modes */
+        'multi_user'     => true,        // not yet implemented
+        'federated_uri'  => '',          // not yet implemented
+        'certficate_uri' => ''           // not yet implemented
 );
 
 ?>
