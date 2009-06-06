2531f55a29bc9b4b595e9feb0d7f274772d312a2
phpbbopenid <phpbbopenid@gmail.com>
2009-06-06 13:47:33 +0200

    bnodes

--- tabfriends.php
+++ tabfriends.php
@@ -22,8 +22,8 @@ if (!empty($_REQUEST['webid'])) {
 ?>
                           <table id="friendstable">
                           <tr><td></td><td>Name</span></td><td>URL</td></tr>
-                          <tr typeof="foaf:Person"><td>Add: </td><td><input size="12" id="friend1" property="foaf:name" onChange="makeTags()" type="text" name="friend1name" /></td><td><input size="12" rel="rdfs:seeAlso" id="friend1" onChange="makeTags()" type="text" name="friend1" /></td></tr>
-                          <tr typeof="foaf:Person"><td>Add: </td><td><input size="12" id="friend2" property="foaf:name" onChange="makeTags()" type="text" name="friend2name" /></td><td><input size="12" rel="rdfs:seeAlso" id="friend2" onChange="makeTags()" type="text" name="friend2" /></td></tr>
+                          <tr typeof="foaf:Person"><td>Add: </td><td><input size="12" id="friend1" property="foaf:name" onChange="makeTags()" type="text" name="friend1name" /></td><td><input size="12" rel="rdfs:seeAlso" id="friend1" onChange="makeTags()" type="text" name="friend1" about="friend1" /></td></tr>
+                          <tr typeof="foaf:Person"><td>Add: </td><td><input size="12" id="friend2" property="foaf:name" onChange="makeTags()" type="text" name="friend2name" /></td><td><input size="12" rel="rdfs:seeAlso" id="friend2" onChange="makeTags()" type="text" name="friend2" about="friend2" /></td></tr>
                           </table>
                           <a href="#" onclick="javascript:addf()">Add</a>
 
