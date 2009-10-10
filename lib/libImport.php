c2b874b24f95ac27e81115c861540e55aca1a567
Akbar Hossain <akkiehossain@googlemail.com>
2009-10-10 23:25:49 +0100

    removed php warnings

--- lib/libImport.php
+++ lib/libImport.php
@@ -12,11 +12,11 @@
 function getImport()
 {
 
-	$result = array('referer' => $_SERVER['HTTP_REFERER']);
+	$result = array('referer' => (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : NULL ));
 
         // http://irc.sioc-project.org/users/PovAddict
 	// if starts with http://irc.sioc-project.org/users/ populate nick and holdsAccount
-	if ( strpos($_SERVER['HTTP_REFERER'], 'http://irc.sioc-project.org/users/') === 0 ) {
+	if ( (isset($_SERVER['HTTP_REFERER'])) && ( strpos($_SERVER['HTTP_REFERER'], 'http://irc.sioc-project.org/users/') === 0 ) ) {
 		$result['holdsAccount'] = $_SERVER['HTTP_REFERER'] . "#user";
 		$result['nick'] = str_replace('http://irc.sioc-project.org/users/', '',  $_SERVER['HTTP_REFERER']) ;
 	}
