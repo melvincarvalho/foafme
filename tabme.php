9f2d60fac37ff1616caa93dbcd9825a0de39299d
Akbar Hossain <akkiehossain@googlemail.com>
2009-10-10 23:48:29 +0100

    remove php warnings

--- tabme.php
+++ tabme.php
@@ -2,7 +2,7 @@
 
 require_once('lib/libAuthentication.php');
 
-$auth = $_SESSION['auth'];
+$auth = isset($_SESSION['auth']) ? $_SESSION['auth'] : NULL;
 
 if (!empty($_REQUEST['webid'])) {
   $auth = get_agent($_REQUEST['webid']);
@@ -24,7 +24,7 @@ if (!empty($_REQUEST['webid'])) {
 
 			<table>
 			    <tr><td><b>Create Profile!</b> </td><td></tr>
-			    <tr><td>Username/Nick:</td><td><input id="nick" onChange="makeTags()" property="foaf:nick" type="text" name="nick" value="<?= $import['nick'] ?>" /><span class="required">*</span></tr>
+			    <tr><td>Username/Nick:</td><td><input id="nick" onChange="makeTags()" property="foaf:nick" type="text" name="nick" value="<?= isset($import['nick']) ? $import['nick'] : NULL ?>" /><span class="required">*</span></tr>
 			    <tr><td>First Name</td><td><input property="foaf:firstName" id="firstname" onChange="makeTags()" type="text" name="firstName"></td></tr>
 			    <tr><td>Last Name</td><td><input property="foaf:givenName" id="surname" onChange="makeTags()" type="text" name="surname"></td></tr>
 			    <tr><td>Picture</td><td><input rel="foaf:depiction" id="depiction" onChange="makeTags()" type="text" name="depiction"></td></tr>
