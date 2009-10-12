c173d718b19a38524200d2dd5b3d3b128b237b26
Akbar Hossain <akkiehossain@googlemail.com>
2009-10-12 22:05:17 +0100

    Added license details

--- lib/libActivity.php
+++ lib/libActivity.php
@@ -1,5 +1,31 @@
 <?
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+//
+// Filename   : libActivity.php                                                                                                  
+// Version    : 0.1
+// Date       : 12th October 2009
+//
+// Copyright 2008-2009 foaf.me
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// "Everything should be made as simple as possible, but no simpler."
+// -- Albert Einstein
+//
+//-----------------------------------------------------------------------------------------------------------------------------------
+
 function get_openlink_acct($acct)
 {
 	$parser = ARC2::getRDFParser();
