9844ac7fbb2a8ca493eec6c3a4fdc9291d20743c
Akbar Hossain <akkiehossain@googlemail.com>
2009-10-15 22:26:41 +0100

    Added license details

--- tabme.php
+++ tabme.php
@@ -1,5 +1,30 @@
 <?php
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+//
+// Filename   : tabme.php                                                                                                  
+// Date       : 15th October 2009
+//
+// Copyright 2008-2009 foaf.me
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// "Everything should be made as simple as possible, but no simpler."
+// -- Albert Einstein
+//
+//-----------------------------------------------------------------------------------------------------------------------------------
+
 require_once('lib/libAuthentication.php');
 
 $auth = isset($_SESSION['auth']) ? $_SESSION['auth'] : NULL;
