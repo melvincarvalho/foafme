d8783bbe388a10246a73ba0cf83f4977246e5975
melvincarvalho <melvincarvalho@gmail.com>
2009-11-26 02:09:05 +0000

    cleanup

--- head.php
+++ head.php
@@ -57,9 +57,9 @@ $webid = "";
 
 /* Reconstructs the signed message: the URI except the 'sig' parameter */
 $full_uri = ((isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on")) ? "https" : "http")
-. "://" . $_SERVER["HTTP_HOST"]
-. ($_SERVER["SERVER_PORT"] != ((isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on")) ? 443 : 80) ? ":".$_SERVER["SERVER_PORT"] : "")
-. $_SERVER["REQUEST_URI"];
+    . "://" . $_SERVER["HTTP_HOST"]
+    . ($_SERVER["SERVER_PORT"] != ((isset($_SERVER["HTTPS"]) && ($_SERVER["HTTPS"] == "on")) ? 443 : 80) ? ":".$_SERVER["SERVER_PORT"] : "")
+    . $_SERVER["REQUEST_URI"];
 
 $signed_info = substr($full_uri, 0, -5-strlen(urlencode(isset($_GET["sig"]) ? $_GET["sig"] : NULL)));
 
@@ -72,38 +72,38 @@ if ($sigalg == "rsa-sha1") {
          * Loads the trusted certificate of the IdP: its public key is used to
          * verify the integrity of the signed assertion.
          */
-        $fp = fopen($idp_certificate, "r");
-        $cert = fread($fp, 8192);
-        fclose($fp);
-
-        $pubkeyid = openssl_get_publickey($cert);
-
+    $fp = fopen($idp_certificate, "r");
+    $cert = fread($fp, 8192);
+    fclose($fp);
+    
+    $pubkeyid = openssl_get_publickey($cert);
+    
         /* Verifies the signature */
-        $verified = openssl_verify($signed_info, $signature, $pubkeyid);
-        if ($verified == 1) {
-                // The verification was successful.
-                $webid = $_GET["webid"];
-		$agent = $_GET["webid"];
-		$loggedIn = true;
-
-        } elseif ($verified == 0) {
-                // The signature didn't match.
-                $webid = "";
-        } else {
-                // Error during the verification.
-                $webid = "";
-        }
-
-        openssl_free_key($pubkeyid);
-} else {
-        // Unsupported signature algorithm.
+    $verified = openssl_verify($signed_info, $signature, $pubkeyid);
+    if ($verified == 1) {
+    // The verification was successful.
+        $webid = $_GET["webid"];
+        $agent = $_GET["webid"];
+        $loggedIn = true;
+        
+    } elseif ($verified == 0) {
+    // The signature didn't match.
         $webid = "";
+    } else {
+    // Error during the verification.
+        $webid = "";
+    }
+    
+    openssl_free_key($pubkeyid);
+} else {
+// Unsupported signature algorithm.
+    $webid = "";
 }
 $webidbase = preg_replace('/#.*/', '', $webid);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
 <!--
  * FOAF Me : FOAF Me Home Page and FOAF creator wizard.
  * Copyright (c) http://foaf.me/
@@ -111,213 +111,213 @@ $webidbase = preg_replace('/#.*/', '', $webid);
  *
 -->
 <html xmlns="http://www.w3.org/1999/xhtml"
-  xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
-  xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
-  xmlns:dc="http://purl.org/dc/elements/1.1/"
-  xmlns:dcterms="http://purl.org/dc/terms/"
-  xmlns:foaf="http://xmlns.com/foaf/0.1/"
-  xmlns:vcard="http://www.w3.org/2006/vcard/ns#"
-  xmlns:biografr="http://biografr.com/ontology#">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="Content-Style-Type" content="text/css" />
-<meta http-equiv="Content-Script-Type" content="text/javascript" />
-<!-- <base href="<?= $agent ?>"> -->
-<title>FOAF Me</title>
-<link rel="stylesheet" href="css/jquery.tabs.css" type="text/css" media="print, projection, screen" />
-<!--
-<link rel="shortcut icon" href="/img/favicon.png" type="image/png" />	
--->
-<link rel="stylesheet" type="text/css" title="default" href="css/main.css" />
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<script type="text/javascript" src="js/jquery/1.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/jquery/jquery.tabs.pack.js"></script>
-<script type="text/javascript" src="js/jquery.rdfquery.core-1.0.js"></script>
-<script type="text/javascript" src="js/jquery.rdfquery.rdfa-1.0.js"></script>
-<script type="text/javascript" src="js/jquery.rdfquery.rules-1.0.js"></script>
-<script type="text/javascript" src="js/jquery.editinplace.js"></script>
-<script type="text/javascript" src="js/jquery.json-1.3.js"></script>
+      xmlns:xsd="http://www.w3.org/2001/XMLSchema#"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:dcterms="http://purl.org/dc/terms/"
+      xmlns:foaf="http://xmlns.com/foaf/0.1/"
+      xmlns:vcard="http://www.w3.org/2006/vcard/ns#"
+      xmlns:biografr="http://biografr.com/ontology#">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <meta http-equiv="Content-Script-Type" content="text/javascript" />
+        <!-- <base href="<?= $agent ?>"> -->
+        <title>FOAF Me</title>
+        <link rel="stylesheet" href="css/jquery.tabs.css" type="text/css" media="print, projection, screen" />
+        <!--
+        <link rel="shortcut icon" href="/img/favicon.png" type="image/png" />	
+        -->
+        <link rel="stylesheet" type="text/css" title="default" href="css/main.css" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <script type="text/javascript" src="js/jquery/1.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery/jquery.tabs.pack.js"></script>
+        <script type="text/javascript" src="js/jquery.rdfquery.core-1.0.js"></script>
+        <script type="text/javascript" src="js/jquery.rdfquery.rdfa-1.0.js"></script>
+        <script type="text/javascript" src="js/jquery.rdfquery.rules-1.0.js"></script>
+        <script type="text/javascript" src="js/jquery.editinplace.js"></script>
+        <script type="text/javascript" src="js/jquery.json-1.3.js"></script>
 
 
-<link rel="stylesheet" href="jquery.tabs-ie.css" type="text/css" media="projection, screen" />
-	
-	
-<script type="text/javascript">
-// <!--
-
-/* JQuery Script */
-/*****************/       
-
-// Initialise page
-$(function() {
-	$('#container').tabs({ fxFade: true, fxSpeed: 'fast' });
-	gGeneratorAgent = 'http://<?php echo $_SERVER['HTTP_HOST'] ?>';
-	gErrorReportsTo = 'mailto:error@<?php echo $_SERVER['HTTP_HOST'] ?>';
-	sparul();
-	makeTags();
-});
+        <link rel="stylesheet" href="jquery.tabs-ie.css" type="text/css" media="projection, screen" />
 
 
-// TODO: make this more generic
-function sparul() {
-	$("span[property]").editInPlace({ url: 'sparul.php' , params: 'uri=<?= $agent ?>' });
-	$("span[rel]").editInPlace({ url: 'sparul.php' , params: 'uri=<?= $agent ?>' });
-}
+        <script type="text/javascript">
+            // <!--
 
-// TODO: make this more generic
-function adda() {
-  $("#accounts tr:last").clone().appendTo("#accountstable");
-}
+            /* JQuery Script */
+            /*****************/       
 
-// TODO: make this more generic
-function addi() {
-  $("#interests tr:last").clone().appendTo("#intereststable");
-}
-
-// TODO: make this more generic
-function del(el) {
-	str = "#" + el;
-	frag = $(str).parent().parent().rdf().databank.triples()[0].subject.value;
-
-	var re = new RegExp("[0-9A-Za-z]*$","ig");
-    	var resultArray = re.exec(frag);
-
-	while (resultArray) {
-		frag = resultArray[0];
-		resultArray = re.exec(str);
-	}
-
-	frag = "<?= $agent ?>#" + frag;
-	//alert("sparul.php?uri=<?= $agent ?>&delete=" + escape(frag));
-	//$.post("sparul.php?uri=<?= $agent ?>&delete=" + escape(frag));
-
-	var sparul = 'DELETE { ';
-	$(str).parent().parent().rdf().databank.triples().each(function () { sparul += this + ' '; } );
-	sparul += ' } ';
-	//alert('DELETE:  This funcionality is in Alpha ' + sparul);
-	jQuery.post( '<?= $_REQUEST['webid'] ?>', sparul );
-
-	$(str).parent().parent().remove();
-	//location.reload();
-}
+            // Initialise page
+            $(function() {
+                $('#container').tabs({ fxFade: true, fxSpeed: 'fast' });
+                gGeneratorAgent = 'http://<?php echo $_SERVER['HTTP_HOST'] ?>';
+                gErrorReportsTo = 'mailto:error@<?php echo $_SERVER['HTTP_HOST'] ?>';
+                sparul();
+                makeTags();
+            });
 
 
-// TODO: use RDFQuery for this
-function makeTags() {
-	
-	// set uri from nick
-	if ( $("#nick").val() != "" ) {
-		$("#uri").val($("#nick").val());
-	} else {
-		return;
-	}
-
-	// set homepage from nick
-	if ( $("#homepage").val() == "") {
-		$("#homepage").val( "http://<?php echo $_SERVER['HTTP_HOST'] ?>/" + $("#nick").val() );
-		$("#displayname").html( "http://<?php echo $_SERVER['HTTP_HOST'] ?>/" + $("#nick").val() + "#me" );
-		$("#saving").css("display", "inline");;
-	}
-
-    rdf  = '<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n      ' +
-                  'xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n      ' +
-                  'xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n      ' +
-                  'xmlns:rsa=\"http://www.w3.org/ns/auth/rsa#\"\n      ' +
-                  'xmlns:cert=\"http://www.w3.org/ns/auth/cert#\"\n      ' +
-                  'xmlns:admin=\"http://webns.net/mvcb/\">\n';
-
-    // preprocess
-
-    // PersonalProfileDocument
-    $("[typeof=foaf:PersonalProfileDocument]") .each(function (i) {
-	    rdf += makeStartTag(this);
-	    $(this).find("[property],[rel]").each( function(){ rdf += (makeTag(this)) ; }  );
-		rdf += makeEndTag(this);
-
-    });
-
-    // Person #me
-    $("[typeof=foaf:Person]:first").each(function (i) {
-
-	    // Profile
-	    rdf += makeStartTag(this);
-	    $("#me").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
-	    $("#accounts").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
-	    $("#interests").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
-
-		// Friends tab
-		var friends = '';
-		$("#friends [typeof=foaf:Person]").each(function (i) {
-		    var friend = ''
-			$(this).find("[property],[rel]").each( function(){ friend += '' + makeTag(this)? '        ' + makeTag(this):'' ; }  );
-            if (friend) {
-	            friends = '        ' + makeStartTag(this);
-				friends += friend;
-	            friends += '        ' + makeEndTag(this);
-				rdf += '    <foaf:knows>\n';
-				rdf += friends;
-				rdf += '    </foaf:knows>\n';
+            // TODO: make this more generic
+            function sparul() {
+                $("span[property]").editInPlace({ url: 'sparul.php' , params: 'uri=<?= $agent ?>' });
+                $("span[rel]").editInPlace({ url: 'sparul.php' , params: 'uri=<?= $agent ?>' });
             }
-		});
 
-	    rdf += makeEndTag(this);
-    });
+            // TODO: make this more generic
+            function adda() {
+                $("#accounts tr:last").clone().appendTo("#accountstable");
+            }
 
-	// Security tab
-	var id = '';
-	var cert = '';
-	$("#security [typeof]").each(function (i) {
-		$(this).parent().find("[property],[rel]").each( function(){ cert += '' + makeTag(this)? '        ' + makeTag(this):'' ; }  );
-		if (cert) {
-			id += '    ' + makeStartTag(this);
-			id += cert;
-			id += '    ' + makeEndTag(this);
-		}
-	});
-	if (id) {
-		rdf += '<rdf:Description>\n<rdf:type rdf:resource="http://www.w3.org/ns/auth/rsa#RSAPublicKey"/>\n<cert:identity rdf:resource="#me"/>\n';
-		rdf += cert;
-		rdf += '</rdf:Description>\n';
-	}
+            // TODO: make this more generic
+            function addi() {
+                $("#interests tr:last").clone().appendTo("#intereststable");
+            }
 
-    rdf += '</rdf:RDF>\n'
-    //alert(rdf);
-	$("#rdf").val(rdf);
-}
+            // TODO: make this more generic
+            function del(el) {
+                str = "#" + el;
+                frag = $(str).parent().parent().rdf().databank.triples()[0].subject.value;
 
-function makeStartTag(el) {
-    var about = $(el).attr("about") ? $(el).attr("about") : $(el).find("[rel]").val() ;
-    return '<' + $(el).attr("typeof")  + (about?(' rdf:ID="'+about+'"'):(' rdf:about=""'))  + '>\n';
-}
+                var re = new RegExp("[0-9A-Za-z]*$","ig");
+                var resultArray = re.exec(frag);
 
-function makeEndTag(el) {
-    return '</' + $(el).attr("typeof") + '>\n';
-}
+                while (resultArray) {
+                    frag = resultArray[0];
+                    resultArray = re.exec(str);
+                }
+
+                frag = "<?= $agent ?>#" + frag;
+                //alert("sparul.php?uri=<?= $agent ?>&delete=" + escape(frag));
+                //$.post("sparul.php?uri=<?= $agent ?>&delete=" + escape(frag));
+
+                var sparul = 'DELETE { ';
+                $(str).parent().parent().rdf().databank.triples().each(function () { sparul += this + ' '; } );
+                sparul += ' } ';
+                //alert('DELETE:  This funcionality is in Alpha ' + sparul);
+                jQuery.post( '<?= $_REQUEST['webid'] ?>', sparul );
+
+                $(str).parent().parent().remove();
+                //location.reload();
+            }
 
 
-function makeTag(el) {
-    var prop   = $(el).attr("property");
-    var type   = $(el).attr("typeof");
-    var about  = $(el).attr("about");
-    var rel    = $(el).attr("rel");
-	var val    = $(el).val();
-	var inner  = $(el).attr("inner");
-    var href   = $(el).attr("href")?$(el).attr("href"):val;
+            // TODO: use RDFQuery for this
+            function makeTags() {
 
-	if (!val && !href) return '';
-	if (rel)           return '    <' + rel  + ' rdf:resource="'+ href +'"' + '/>\n';
-	if (prop)          return '    <' + prop + ((inner)?' rdf:parseType="Resource"':'') + '>' + (inner?'<'+inner+'>':'') + val + (inner?'</'+inner+'>':'') + '</' + prop + '>' + '\n';
-	if (type)          return '    <' + type + ' rdf:ID="'+ about +'"' + '>' + '</' + type + '>' + '\n';
+                // set uri from nick
+                if ( $("#nick").val() != "" ) {
+                    $("#uri").val($("#nick").val());
+                } else {
+                    return;
+                }
 
-}
+                // set homepage from nick
+                if ( $("#homepage").val() == "") {
+                    $("#homepage").val( "http://<?php echo $_SERVER['HTTP_HOST'] ?>/" + $("#nick").val() );
+                    $("#displayname").html( "http://<?php echo $_SERVER['HTTP_HOST'] ?>/" + $("#nick").val() + "#me" );
+                    $("#saving").css("display", "inline");;
+                }
 
-// -->
-</script>
+                rdf  = '<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n      ' +
+                    'xmlns:rdfs=\"http://www.w3.org/2000/01/rdf-schema#\"\n      ' +
+                    'xmlns:foaf=\"http://xmlns.com/foaf/0.1/\"\n      ' +
+                    'xmlns:rsa=\"http://www.w3.org/ns/auth/rsa#\"\n      ' +
+                    'xmlns:cert=\"http://www.w3.org/ns/auth/cert#\"\n      ' +
+                    'xmlns:admin=\"http://webns.net/mvcb/\">\n';
 
-	
-	
+                // preprocess
 
-</head>
+                // PersonalProfileDocument
+                $("[typeof=foaf:PersonalProfileDocument]") .each(function (i) {
+                    rdf += makeStartTag(this);
+                    $(this).find("[property],[rel]").each( function(){ rdf += (makeTag(this)) ; }  );
+                    rdf += makeEndTag(this);
+
+                });
+
+                // Person #me
+                $("[typeof=foaf:Person]:first").each(function (i) {
+
+                    // Profile
+                    rdf += makeStartTag(this);
+                    $("#me").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
+                    $("#accounts").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
+                    $("#interests").find("[property],[rel]").each( function(){ rdf += (makeTag(this)) }  );
+
+                    // Friends tab
+                    var friends = '';
+                    $("#friends [typeof=foaf:Person]").each(function (i) {
+                        var friend = ''
+                        $(this).find("[property],[rel]").each( function(){ friend += '' + makeTag(this)? '        ' + makeTag(this):'' ; }  );
+                        if (friend) {
+                            friends = '        ' + makeStartTag(this);
+                            friends += friend;
+                            friends += '        ' + makeEndTag(this);
+                            rdf += '    <foaf:knows>\n';
+                            rdf += friends;
+                            rdf += '    </foaf:knows>\n';
+                        }
+                    });
+
+                    rdf += makeEndTag(this);
+                });
+
+                // Security tab
+                var id = '';
+                var cert = '';
+                $("#security [typeof]").each(function (i) {
+                    $(this).parent().find("[property],[rel]").each( function(){ cert += '' + makeTag(this)? '        ' + makeTag(this):'' ; }  );
+                    if (cert) {
+                        id += '    ' + makeStartTag(this);
+                        id += cert;
+                        id += '    ' + makeEndTag(this);
+                    }
+                });
+                if (id) {
+                    rdf += '<rdf:Description>\n<rdf:type rdf:resource="http://www.w3.org/ns/auth/rsa#RSAPublicKey"/>\n<cert:identity rdf:resource="#me"/>\n';
+                    rdf += cert;
+                    rdf += '</rdf:Description>\n';
+                }
+
+                rdf += '</rdf:RDF>\n'
+                //alert(rdf);
+                $("#rdf").val(rdf);
+            }
+
+            function makeStartTag(el) {
+                var about = $(el).attr("about") ? $(el).attr("about") : $(el).find("[rel]").val() ;
+                return '<' + $(el).attr("typeof")  + (about?(' rdf:ID="'+about+'"'):(' rdf:about=""'))  + '>\n';
+            }
+
+            function makeEndTag(el) {
+                return '</' + $(el).attr("typeof") + '>\n';
+            }
+
+
+            function makeTag(el) {
+                var prop   = $(el).attr("property");
+                var type   = $(el).attr("typeof");
+                var about  = $(el).attr("about");
+                var rel    = $(el).attr("rel");
+                var val    = $(el).val();
+                var inner  = $(el).attr("inner");
+                var href   = $(el).attr("href")?$(el).attr("href"):val;
+
+                if (!val && !href) return '';
+                if (rel)           return '    <' + rel  + ' rdf:resource="'+ href +'"' + '/>\n';
+                if (prop)          return '    <' + prop + ((inner)?' rdf:parseType="Resource"':'') + '>' + (inner?'<'+inner+'>':'') + val + (inner?'</'+inner+'>':'') + '</' + prop + '>' + '\n';
+                if (type)          return '    <' + type + ' rdf:ID="'+ about +'"' + '>' + '</' + type + '>' + '\n';
+
+            }
+
+            // -->
+        </script>
+
+
+
+
+    </head>
 
 <? $headerLoaded = true ?>
