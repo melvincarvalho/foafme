c19b7dc38a01bb60525d5424621bbc810bf34320
melvincarvalho <melvincarvalho@gmail.com>
2009-11-26 13:47:01 +0000

    cleanup

--- header.php
+++ header.php
@@ -16,51 +16,48 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * "Everything should be made as simple as possible, but no simpler." 
+ *
+ * "Everything should be made as simple as possible, but no simpler."
  * -- Albert Einstein
  *
  */
 ?>
 
 <body id="tools_scrollable">
-	<div id="wrap">	
-<a href="index.php">Home</a> | <a href="about.php">About</a> | <a href="documentation.php">Documentation</a> | <a href="downloads.php">Downloads</a> | <a href="http://groups.google.com/group/foafme?lnk=srg&amp;hl=en&amp;ie=UTF-8&amp;oe=utf-8">Mailing List</a> 
+    <div id="wrap">
+        <a href="index.php">Home</a> | <a href="http://groups.google.com/group/foafme?lnk=srg&amp;hl=en&amp;ie=UTF-8&amp;oe=utf-8">Mailing List</a>
 
-<?php
-//if (!empty($_SESSION['auth']) && $_SESSION['auth']['subjectAltName']) {
+        <?php
+        //if (!empty($_SESSION['auth']) && $_SESSION['auth']['subjectAltName']) {
 
-// If logged in
-if (!empty($webid) ) {
-?>
-		<div id="user">
-				<a id="logout" href="http://foaf.me/clearSession.php?return_to=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]" ?>">
-					<!-- Logout <strong><?= $_SESSION['auth']['subjectAltName'] ?> </strong> -->
+        // If logged in
+        if (!empty($webid) ) {
+            ?>
+        <div id="user">
+            <a id="logout" href="http://foaf.me/clearSession.php?return_to=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]" ?>">
+                    <!-- Logout <strong><?= $_SESSION['auth']['subjectAltName'] ?> </strong> -->
 		Logout <strong><?= $_REQUEST['webid'] ?></strong>
-				</a>
-			<br/>
-		</div>
-<? } else { ?>
-		<div id="user">
-				<a id="account" href="https://foafssl.org/srv/idp?authreqissuer=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]" ?>">
+            </a>
+            <br/>
+        </div>
+        <? } else { ?>
+        <div id="user">
+            <a id="account" href="https://foafssl.org/srv/idp?authreqissuer=<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]" ?>">
 					login to your <strong>account</strong>
-				</a>
-			<br />
-		</div>
-<? } ?>		
-		
-		
-		 
-		
-		
-		<div id="content"> 
-		
-		<h1>
-			<img alt="foaf" src="images/foaf.gif" />
-			<strong>FOAF</strong> 
-		</h1>
-		 
-		<h2></h2>
+            </a>
+            <br />
+        </div>
+        <? } ?>
+
+
+        <div id="content">
+
+            <h1>
+                <img alt="foaf" src="images/foaf.gif" />
+                <strong>FOAF</strong>
+            </h1>
+
+            <h2></h2>
 
 
 
