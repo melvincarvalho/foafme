7ae7504a29fcc2ce80ae70ec5f8c114a43a7949e
melvincarvalho <melvincarvalho@gmail.com>
2009-12-28 20:33:42 +0100

    #46 visualisations

--- tabme.php
+++ tabme.php
@@ -51,10 +51,10 @@ if ( $auth['isAuthenticated'] == 1 || !empty($_REQUEST['webid']) ) {
             </script>
         <?php
         print "<h2>Other Visualisations</h2>";
-        print "<a href='http://foafmap.net/?foaf='" . $webid . "'>Foaf MAP</a><br/>";
-        print "<a href='http://www.foafer.org/?file='" . $webid . "'>Foafer</a><br/>";
-        print "<a href='http://xml.mfd-consult.dk/foaf/explorer/?foaf='" . $webid . "'>Foaf Explorer</a><br/>";
-        print "<a href='http://dataviewer.zitgist.com/?uri='" . $webid . "'>Zitgist</a><br/>";
+        print "<a href='http://foafmap.net/?foaf=" . $webid . "'>Foaf MAP</a><br/>";
+        print "<a href='http://www.foafer.org/?file=" . $webid . "'>Foafer</a><br/>";
+        print "<a href='http://xml.mfd-consult.dk/foaf/explorer/?foaf=" . $webid . "'>Foaf Explorer</a><br/>";
+        print "<a href='http://dataviewer.zitgist.com/?uri=" . $webid . "'>Zitgist</a><br/>";
         ?>
 
 <?php
