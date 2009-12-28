4f5a585dac3165c5e6c46ae1aec01b4611c29fa0
melvincarvalho <melvincarvalho@gmail.com>
2009-12-28 21:59:56 +0100

    #46 visualisations

--- tabme.php
+++ tabme.php
@@ -54,9 +54,9 @@ if ( $auth['isAuthenticated'] == 1 || !empty($_REQUEST['webid']) ) {
         print "<a target='_blank' href='http://www.foafer.org/?file=" . $webid . "'>Foafer</a><br/>";
         print "<a target='_blank' href='http://www5.wiwiss.fu-berlin.de/marbles?uri=" . $webid . "'>Marbles</a><br/>";
         print "<a target='_blank' href='http://sig.ma/search?q=" . $webid . "'>Sig.ma</a><br/>";
+        print "<a target='_blank' href='http://linkeddata.uriburner.com/ode/?uri=" . $webid . "'>OpenLink Data Explorer</a><br/>";
         print "<a target='_blank' href='http://xml.mfd-consult.dk/foaf/explorer/?foaf=" . $webid . "'>Foaf Explorer</a><br/>";
         print "<a target='_blank' href='http://dataviewer.zitgist.com/?uri=" . $webid . "'>Zitgist</a><br/>";
-        print "<a target='_blank' href='http://linkeddata.uriburner.com/ode/?uri=" . $webid . "'>URI Burner</a><br/>";
         print "<a target='_blank' href='http://foafmap.net/?foaf=" . $webid . "'>Foaf MAP</a><br/>";
         ?>
 
