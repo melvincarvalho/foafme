415cd6a986438bc77f6e0fc463b1957c3f10d2d5
Laszlo T <ltorokjr@gmail.com>
2010-02-01 01:13:44 +0100

    added rsync wrapper script

---
+++ deploy2www.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+if [ $# -lt 2 ]
+then
+    echo ""
+    echo "Usage: $0 [src-dir] [dst-dir]"
+    echo ""
+    echo "Example: ./$0 . relay@relay.me:~/www/foaf.me/"
+    echo ""
+    echo "NOTE:"
+    echo "Files(patterns) in ./rsync_exclude.txt will not be transferred."
+else
+    $exclude_list
+    if test -f rsync_exclude.txt
+    then
+        $exclude_list="--exclude-from=rsync_exclude.txt"
+    fi
+    rsync -vrR $exclude_list -e "ssh -p22" $1 $2;
+fi
