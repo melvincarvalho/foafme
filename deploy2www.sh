975a797c26a952a08a252316e3180d44e4ff23cf
Laszlo T <ltorokjr@gmail.com>
2010-02-02 23:36:28 +0100

    renamed rsync_exclude

--- deploy2www.sh
+++ deploy2www.sh
@@ -9,9 +9,9 @@ then
     echo "NOTE:"
     echo "Files(patterns) in ./rsync_exclude.txt will not be transferred."
 else
-    if test -f rsync_exclude.txt
+    if test -f rsync_exclude
     then
-        exclude_list="--exclude-from=rsync_exclude.txt"
+        exclude_list="--exclude-from=rsync_exclude"
     fi
     rsync --progress -azC --force --delete $exclude_list -e "ssh -p22 -i ~/.ssh/id_rsa" $1 $2;
 fi
