f871aa8326a107c17c47712e94e35ed32d4ef5fb
Melvin Carvalho <melvincarvalho@gmail.com>
2010-10-09 01:39:57 +0200

    close #177

---
+++ entry.php
@@ -0,0 +1,26 @@
+<?
+require_once('v1/authenticationLevel.php');
+require_once('v1/documentation.php');
+
+page_header("FOAF+SSL Simple Login Page");
+
+?>
+<body>
+<h1>FOAF+SSL Entry Page</h1>
+
+
+<div><a href="https://foaf.me/simpleLogin.php">Click Here for the FOAF+SSL Test End Point</a></div>
+<br/>
+<h3>The FOAF+SSL User Process</h3>
+<div id="sequence"><img src="images/key3.jpg"/></div>
+
+<table width=100% id=section><tr><td><B>See Also</B></td>
+</table>
+<ul>
+<a href="http://foaf.me/Enabling_SSL_Client_Certificates_on_Apache.php">Enabling SSL Client Certificates on Apache</a><br>
+<a href="http://foaf.me/Using_PHP_to_create_X.509_Client_Certificates.php">Using PHP to create X.509 Client Certificates</a><br>
+<a href="https://foaf.me/RDF_Representation_of_a_X.509_Client_Certificate.php">RDF Representation of a X.509 Client Certificate</a><br>
+</ul>
+</body>
+</html>
+
